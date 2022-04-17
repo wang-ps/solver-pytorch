02dcaeeb1556d73ea3eb8dc6baac3738d806d159
wang-ps <wangps@hotmail.com>
2022-04-17 15:11:37 +0800

    Refine dataset.py

--- dataset.py
+++ dataset.py
@@ -11,17 +11,21 @@ def read_file(filename):
 
 
 class Dataset(torch.utils.data.Dataset):
-  def __init__(self, root, filelist, transform, read_file=read_file, in_memory=True):
+
+  def __init__(self, root, filelist, transform, read_file=read_file,
+               in_memory=False, take: int = -1):
     super(Dataset, self).__init__()
     self.root = root
     self.filelist = filelist
     self.transform = transform
     self.in_memory = in_memory
     self.read_file = read_file
+    self.take = take
+
     self.filenames, self.labels = self.load_filenames()
     if self.in_memory:
       print('Load files into memory from ' + self.filelist)
-      self.samples = [self.read_file(f)
+      self.samples = [self.read_file(os.path.join(self.root, f))
                       for f in tqdm(self.filenames, ncols=80, leave=False)]
 
   def __len__(self):
@@ -29,9 +33,10 @@ class Dataset(torch.utils.data.Dataset):
 
   def __getitem__(self, idx):
     sample = self.samples[idx] if self.in_memory else \
-             self.read_file(self.filenames[idx])
+             self.read_file(os.path.join(self.root, self.filenames[idx]))  # noqa
     output = self.transform(sample, idx)    # data augmentation + build octree
     output['label'] = self.labels[idx]
+    output['filename'] = self.filenames[idx]
     return output
 
   def load_filenames(self):
@@ -42,6 +47,11 @@ class Dataset(torch.utils.data.Dataset):
       tokens = line.split()
       filename = tokens[0]
       label = tokens[1] if len(tokens) == 2 else 0
-      filenames.append(os.path.join(self.root, filename))
+      filenames.append(filename)
       labels.append(int(label))
-    return filenames, labels
+
+    num = len(filenames)
+    if self.take > num or self.take < 1:
+      self.take = num
+
+    return filenames[:self.take], labels[:self.take]
